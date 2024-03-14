a4f5c1757bd28f3e433da6d094d1bd218c65afff
Helmatron <ema.matro@gmail.com>
2024-03-14 18:37:26 +0100

    fix: riordino

--- js/app.js
+++ js/app.js
@@ -14,15 +14,15 @@ const resto5 = num % 5 // multipli di 5
 
 
 if (resto3 === 0 && resto5 === 0) // Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-{
-    console.log('FizzBuzz')
+{    console.log('FizzBuzz')
+
 } else if (resto3 === 0) {
         console.log('Fizz') // per i multipli di 3 stampi “Fizz” al posto del numero
+
 } else if (resto5 === 0) {
         console.log('Buzz') // per i multipli di 5 stampi “Buzz” al posto del numero.
-} else {
-    console.log(num) // in tutti gli altri casi venga stampato il numero stesso
 
+} else {    console.log(num) // in tutti gli altri casi venga stampato il numero stesso
 } 
  }
 
